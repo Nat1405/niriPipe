0cd568ab646ee01507536f2f105031a8eff3a6e4
Nat1405 <nathanc1945@gmail.com>
2021-02-18 08:37:19 -0800

    Adds default configuration file and int test.
    Creates a default configuration file and makes sure it's accessible with
    an int test.

--- tests/test_niriPipe.py
+++ tests/test_niriPipe.py
@@ -1,7 +1,19 @@
 import subprocess
+import pkg_resources
+import os
+import configparser
 
 
 def test_console_help():
     result = subprocess.run(['niriPipe', '--help'], stdout=subprocess.PIPE)
     assert b'NIRI' in result.stdout
     assert result.returncode == 0
+
+
+def test_default_config_present():
+    config = configparser.ConfigParser()
+    config.read(pkg_resources.resource_filename(
+                'niriPipe',
+                os.path.join('cfg', 'default_config.cfg')))
+
+    assert 'DATARETRIEVAL' in config.sections()
