0cda163674abc981faf4d67432f4251059bfe8d6
Nat1405 <nathanc1945@gmail.com>
2021-02-18 08:20:34 -0800

    Updates setup.py and adds setup.cfg.

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-NAME = 'niriPipe'
-PACKAGE_DATA = {
-    '': ['*.dat', '*.cfg', '*.fits', '*.txt']
-}
-
-setuptools.setup(
-    name=NAME,
-    version="0.0.1",
-    author="Nat Comeau",
-    author_email="ncomeau@uvic.ca",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/nat1405/niriPipe",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    package_data=PACKAGE_DATA,
-    scripts=['bin/niriPipe']
-)
+if __name__ == "__main__":
+    setup()
